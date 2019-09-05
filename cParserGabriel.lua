58d02e036b71de442009ee6dff42247f3080180f
unknown <gabrielsantossilveira@hotmail.com>
2019-09-05 16:52:39 -0300

    initial commit

--- cParserGabriel.lua
+++ cParserGabriel.lua
@@ -4,12 +4,10 @@ local util = require'util'
 
 local s = [[
     s       <-  (Atrib / Exp)* (!.)^ErrEOF
-    --Exp     <-  Fator ((Mais / Mult)^s^ErrSinal Fator)*
     Exp     <-  Fator ((Mais / Mult) Fator^ErrFator)*
     Fator   <-  Float / Int / ExpPar / Var
     ExpPar  <-  '(' Exp^ErrExpParEsq ')'^ErrFaltaParDir
     Var     <-  Letra (Digito / Letra)*
-    --Var     <-  Id (Int Id?)*
     Letra   <-  [a-zA-Z] 
     Digito  <-  [0-9]
     Int     <-  Digito+ (!Letra)^ErrFator
@@ -19,7 +17,6 @@ local s = [[
     Igual   <-  '=' 
     Atrib   <-  Var Igual Exp
     ErrExpParEsq <- (!')' .)*
-    --ErrFaltaParDir <- ''
 ]] 
 
          
@@ -27,12 +24,4 @@ g, lab, pos = m.match(s)
 print(g, lab, pos)
 --gerar o parser
 local p = coder.makeg(g, 'ast')
-
---rodar os testes 'yes'
-local dir = lfs.currentdir() .. '/test/c89/test/yes/'
-util.testYes(dir, 'c', p)
-
---rodar os testes 'no'
-local dir = lfs.currentdir() .. '/test/c89/test/no/'
-util.testNo(dir, 'c', p)
 
